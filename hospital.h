10b7d4533e06af3e699aaa13206750007bf6014d
Kayke Rodrigues de Oliveira <kaykerodr5@gmail.com>
2025-11-11 17:15:26 -0300

    Add files via upload

---
+++ hospital.h
@@ -0,0 +1,20 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+#include <iostream>
+#include "paciente.h"
+
+class Hospital {
+private:
+    int _capacidade;
+    int _qtdPacientes;
+    std::string _nome;
+    Paciente* _paciente;
+public:
+    Hospital(std::string nome);
+    ~Hospital();
+    void cadastrarPaciente(Paciente *p);
+    void removerPaciente(int idPaciente);
+    Paciente* buscarPaciente(int idPaciente);
+    std::string get_nome();
+};
+#endif
