f5a5e3cccac2ee79e22eed180e87662b49970120
Tiago Garcia <tiagog2006@gmail.com>
2025-11-22 10:59:33 -0300

    Adição da função Previsão

--- sensor.cpp
+++ sensor.cpp
@@ -4,12 +4,9 @@
 Sensor::Sensor(std::string tipo, double valor, std::string unidade, double min, double max)
     : _tipo(tipo), _valor(valor), _unidade(unidade), _min(min), _max(max) {}
 
-Sensor::~Sensor()
-{
+Sensor::~Sensor(){
 }
-void Sensor::set_tipo()
-{
-}
+    //Gets
 double Sensor::get_valor()
 {
     return _valor;
@@ -33,10 +30,45 @@ std::string Sensor::get_tipo()
     return _tipo;
 }
 
-void Sensor::set_valor(double novoValor)
-{
+    //Sets
+void Sensor::set_valor(double novoValor){
+    _historico.push_back(_valor);
     _valor = novoValor;
 }
+
+void Sensor::set_tipo(std::string newTipo){
+    _tipo = newTipo;
+}
+
+    //Metodos
+
+
+double Sensor::previsao(){ // Retorna uma media baseada nos cinco ultimos valores do paciente
+    double soma =0;
+
+    if(_historico.size() < 5){
+        for(int i = 0; i < _historico.size(); i++){
+            soma+=_historico[i];
+        }
+        soma += _valor;
+
+        if(soma < _min){
+            std::cout << "Alerta! Previsao de Estado de Emergencia! Tendencia de queda abaixo do limite segur no parametro: " << _tipo << std::endl;
+        }
+        else if(soma > _max){
+            std::cout << "Alerta! Previsao de Estado de Emergencia! Tendencia de pico acima do limite seguro no parametro: " << _tipo << std::endl;
+        }
+
+        return soma / (_historico.size() + 1);
+    }
+
+    for(int i = 1; i < 5; i++){
+        soma += *(_historico.end()-i);
+    }
+    soma += _valor;
+    return soma / 5;
+}
+
 bool Sensor::alerta() const
 {
     return false;
--- sensor.h
+++ sensor.h
@@ -1,28 +1,38 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 #include <iostream>
+#include <vector>
 
 class Sensor
 {
 protected:
-    std::string _tipo;
-    double _valor;
-    std::string _unidade;
     double _min;
     double _max;
+
+    std::string _tipo;
+    std::string _unidade;
+
+    double _valor;
+    std::vector<double> _historico;
 
 public:
     Sensor(std::string tipo, double valor, std::string unidade, double min, double max);
     ~Sensor();
+
     double get_valor();
     double get_min();
     double get_max();
     std::string get_tipo();
     std::string get_unidade();
-    void set_tipo();
+
+    void set_tipo(std::string newTipo);
     void set_valor(double novoValor);
+
+    double previsao();
+
     virtual bool alerta() const;
     virtual void exibirInfo() const;
     virtual void lerValor() = 0;
+
 };
 #endif
